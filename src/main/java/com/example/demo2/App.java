cb9663d7ca62bee9c969f7c3bdc1ed17700b9570
pinkaldk12 <aldiarr150819@gmail.com>
2022-09-19 09:21:53 +0600

    gfdgdf

---
+++ src/main/java/com/example/demo2/App.java
@@ -0,0 +1,26 @@
+package com.example.demo2;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class
+App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Scene scene = new Scene(AnchorPaneExample.createExample(), 640, 480);
+        String styleSheet = getClass().getResource("/style.css").toExternalForm();
+        scene.getStylesheets().add(styleSheet);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
